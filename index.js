3119a8ce94fd61a1ed0badf6e009cdb62c0fb5ac
Christopher Brown <christopher.p.brown@gmail.com>
2016-02-18 21:26:54 -0700

    init commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var app = angular.module('myapp', []);
+
+app.controller('mycontroller', function($scope){
+  var obj = {
+    title: "test title",
+    author: "chrisbrown",
+    vote: 0,
+    thumbnail: "https://media.giphy.com/media/ZYi2Lc03nrryw/giphy.gif",
+    text: "Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth tatsoi tomatillo melon azuki bean garlic. Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini."
+  };
+  $scope.posts = [obj];
+});
