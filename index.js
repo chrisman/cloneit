a4680e4359fede6ffa0ade348cd187df4925a0e3
Christopher Brown <christopher.p.brown@gmail.com>
2016-02-18 22:29:50 -0700

    submit post form

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 var app = angular.module('myapp', []);
 
 app.controller('mycontroller', function($scope){
+  $scope.showForm = false;
+
   var obj = {
     title: "test title",
     author: "chrisbrown",
     vote: 0,
-    thumbnail: "https://media.giphy.com/media/ZYi2Lc03nrryw/giphy.gif",
+    url: "https://media.giphy.com/media/ZYi2Lc03nrryw/giphy.gif",
     text: "Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth tatsoi tomatillo melon azuki bean garlic. Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini."
   };
   $scope.posts = [obj];
+
+  $scope.toggleSubmitForm = function(){
+    console.log('togglin');
+    $scope.showForm = !$scope.showForm;
+  }
+
 });
